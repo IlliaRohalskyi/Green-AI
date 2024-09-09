01d3b3829d37edbdff5170dd5f62e1e79db01c5e
vinkom <vincent.komarek@gmail.com>
2024-09-09 11:56:03 +0200

    First commit

---
+++ frontend/src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <v-container fluid>
+    <v-row>
+      <v-col cols="12" md="6" v-for="(item, index) in jsonData" :key="index">
+        <v-card>
+          <v-card-text>{{ item.name }}</v-card-text>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+import jsonData from '@/assets/data.json'; // Beispiel Pfad
+
+export default {
+  data() {
+    return {
+      jsonData
+    };
+  }
+};
+</script>
+
